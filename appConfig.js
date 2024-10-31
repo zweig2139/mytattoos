f6bae33c1136eeca5d3275043fdb63452a346b15
zweig2139 <128163338+zweig2139@users.noreply.github.com>
2024-11-01 00:31:55 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: "All Image",
+  title: "Makes A Tattoo",
   tagline: "All Article & Image",
   description: "All Article & Image",
-  keywords: "cdom,cdom agc,cdom blog",
-  author: "All.com",
+  keywords: "Tattoo,Tattoo Image,Image Tattoo blog",
+  author: "Im tatttos",
   favicon: "https://pugjs.org/images/favicon.ico",
   imageCount: 30,
   removeBadWords: "porn,nude,sex",
